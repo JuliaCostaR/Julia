57f088fbdcfb309697d4bc912dae50f7ad24201f
JuliaCostaR <61120484+JuliaCostaR@users.noreply.github.com>
2020-12-15 18:40:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+if (window.SimpleAnime){
+	new SimpleAnime();
+}
+
+if SimpleForm({
+	form:".formphp",
+	button: "#enviar",
+	erro: "<div id='form-erro'><h2>Erro no envio</h2><p>Um erro ocorreu, tente para o telefone 9999-9999</p></div>"
+	sucesso: "<div id='form-sucesso'><h2>Formulario enviado com sucesso</h2><p>Em breve, entraremos em contato com você.</p></div>"
+})
