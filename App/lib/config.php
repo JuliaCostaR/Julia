d89c13a0594ab2aafd80d28bcaa7aac66e469d38
JuliaCostaR <61120484+JuliaCostaR@users.noreply.github.com>
2021-05-24 00:02:12 -0300

    Add files via upload

---
+++ App/lib/config.php
@@ -0,0 +1,26 @@
+<?php
+
+$config = array(
+
+    'TITULO' => 'Consultas 3Trim',
+
+    'MENU' => array (
+
+        'Consultas' => array (
+            'Países que Falam Português' => 'PaisesFalamPortugues.php',
+            'Cidades Brasileiras e suas Populações' => 'CidadesBrasileirasPop.php',
+            'Paises Sul-Americanos e seus dados' => 'PaisesSul-Americanos.php',
+            'Paises Africanos e seus dados' => 'PaisesAfricanos.php',
+            'Paises Asiáticos e seus dados' => 'PaisesAsiaticos.php',
+            'Paises Europeus e seus dados' => 'PaisesAfricanos.php'
+        )
+    ),
+
+    // Ajuste aqui as configurações do seu banco de dados.
+
+	'HOST' => "localhost",
+	'USUARIO' => "root",
+	'SENHA' => "1234",
+	'BANCO' => "world"
+
+);
